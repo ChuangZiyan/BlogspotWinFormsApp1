67dc05d7d7e87610a02b6a3396c89ca058348809
ChuangZiyan <ziyan.zhuang@h3platform.com>
2023-10-19 11:35:21 +0800

    加入專案檔案。

---
+++ BlogspotWinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Title_TextBox = New System.Windows.Forms.TextBox()
+        Me.Desc_TextBox = New System.Windows.Forms.TextBox()
+        Me.Go_Button = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Title_TextBox
+        '
+        Me.Title_TextBox.Location = New System.Drawing.Point(206, 12)
+        Me.Title_TextBox.Name = "Title_TextBox"
+        Me.Title_TextBox.Size = New System.Drawing.Size(248, 27)
+        Me.Title_TextBox.TabIndex = 0
+        '
+        'Desc_TextBox
+        '
+        Me.Desc_TextBox.Location = New System.Drawing.Point(206, 45)
+        Me.Desc_TextBox.Name = "Desc_TextBox"
+        Me.Desc_TextBox.Size = New System.Drawing.Size(248, 27)
+        Me.Desc_TextBox.TabIndex = 1
+        '
+        'Go_Button
+        '
+        Me.Go_Button.Location = New System.Drawing.Point(12, 12)
+        Me.Go_Button.Name = "Go_Button"
+        Me.Go_Button.Size = New System.Drawing.Size(94, 60)
+        Me.Go_Button.TabIndex = 2
+        Me.Go_Button.Text = "Go"
+        Me.Go_Button.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(112, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 19)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Title"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(112, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(88, 19)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Description"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 19.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(468, 114)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Go_Button)
+        Me.Controls.Add(Me.Desc_TextBox)
+        Me.Controls.Add(Me.Title_TextBox)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Title_TextBox As TextBox
+    Friend WithEvents Desc_TextBox As TextBox
+    Friend WithEvents Go_Button As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ BlogspotWinFormsApp1/Form1.vb
@@ -0,0 +1,67 @@
+﻿Imports System.Net.Http
+Imports System.Net.Http.Headers
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Private Async Sub Go_Button_Click(sender As Object, e As EventArgs) Handles Go_Button.Click
+        Title_TextBox.Text = ""
+        Desc_TextBox.Text = ""
+        Try
+            Using httpClient As New HttpClient()
+
+                'httpClient.DefaultRequestHeaders.Authorization = New AuthenticationHeaderValue("Bearer", bearerToken)
+                'Dim content As New StringContent(jsonRequestData, Encoding.UTF8, "application/json")
+
+                Dim response As HttpResponseMessage = Await httpClient.GetAsync("https://hihihihi1221.blogspot.com/2023/10/2.html")
+
+                If response.IsSuccessStatusCode Then
+                    Dim responseBody As String = Await response.Content.ReadAsStringAsync()
+                    GetContentJson(responseBody)
+                Else
+                    Console.WriteLine("Http Status Code : " & response.StatusCode)
+                End If
+
+            End Using
+
+        Catch ex As Exception
+            Debug.WriteLine(ex)
+
+        End Try
+    End Sub
+
+
+    Public Sub GetContentJson(htmlContent)
+        Dim pattern As String = "_WidgetManager\.(_SetDataContext)\(([\s\S]+?])\)"
+
+        ' Find the match using the regular expression
+        Dim match As Match = Regex.Match(htmlContent, pattern, RegexOptions.IgnoreCase)
+
+        ' Check if a match is found
+        If match.Success Then
+            ' Extract the JSON data
+            Dim jsonText As String = match.Groups(2).Value
+            Debug.WriteLine(jsonText)
+            jsonText = jsonText.Replace("\", "")
+            Dim jsonData As Object = Newtonsoft.Json.JsonConvert.DeserializeObject(jsonText)
+            For Each item In jsonData
+                Debug.WriteLine(item)
+                If item("name") = "view" Then
+                    Dim view_title = item("data")("title").ToString()
+                    Dim view_description = item("data")("description").ToString().Replace("xa0", "")
+                    'view_description = view_description.Replace("0xa0", "")
+
+                    Title_TextBox.Text = view_title
+                    Desc_TextBox.Text = view_description
+                End If
+
+            Next
+
+        Else
+            ' No match found
+            Debug.WriteLine("JSON data not found in the HTML content.")
+        End If
+
+    End Sub
+
+
+End Class
